21b80ed4e7025f1fbe04ae8627423054b6e62ef1
Ryan <ryanwfortin@gmail.com>
2023-04-06 22:30:11 -0700

    build intro

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,13 +1,14 @@
+import portfolio from "@/data";
 import styles from "@/styles/Intro.module.css";
 
 export default function Intro() {
   return (
     <main className={styles.main}>
-      <div>
-        <img className={styles.profile} src="/profile.jpg" alt="image" />
+      <div className={styles.profile}>
+        <img className={styles.profileImage} src="/profile.jpg" alt="image" />
         <div className={styles.name}>Ryan Fortin</div>
       </div>
-      <p className={styles.bio}>blah blah blah blah blah</p>
+      <p className={styles.bio}>{portfolio.bio}</p>
     </main>
   );
 }
