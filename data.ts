0df770e0dd0b72f39c627572ef576311aceb90af
Ryan <ryanwfortin@gmail.com>
2023-04-06 22:29:54 -0700

    add data

---
+++ data.ts
@@ -0,0 +1,37 @@
+const portfolio = {
+  projects: [{
+    projectName: 'Drafted',
+    url: 'https://github.com/rfll/mocks',
+    techImages: ['/MongoDB.svg', '/ExpressJS.svg', '/React.svg', 'NodeJS.svg'],
+    tech: ['MongoDB', 'ExpressJS', 'ReactJS', 'NodeJS'],
+    about: 'A web app that allows amateur basketball scouts to create a ranking of players entering the NBA.',
+    image: '/drafted.png'
+  },
+  {
+    projectName: 'Parrot Perch',
+    url: 'https://github.com/rfll/parrot-perch',
+    techImages: ['/PostgreSQL.svg', '/ExpressJS.svg', '/React.svg', '/NodeJS.svg'],
+    tech: ['PostgreSQL', 'ExpressJS', 'ReactJS', 'NodeJS'],
+    about: 'A web app with interactive chat rooms where users can watch Twitch and Youtube streams, and play games.',
+    image: '/parrot-perch.png'
+  },
+  {
+    projectName: 'Interview Scheduler',
+    url: 'https://github.com/rfll/scheduler',
+    techImages: ['/React.svg', '/NodeJS.svg', '/axios.png'],
+    tech: ['ReactJS', 'NodeJS', 'Axios'],
+    about: 'A scheduling app that can create, edit, and delete interview appointments.',
+    image: '/scheduler.png'
+  },
+  {
+    projectName: 'Wiki-Maps',
+    url: 'https://github.com/rfll/wiki-maps',
+    techImages: ['/PostgreSQL.svg', '/ExpressJS.svg', '/JavaScript.svg', '/NodeJS.svg'],
+    tech: ['PostgreSQL', 'ExpressJS', 'JavaScript', 'NodeJS'],
+    about: 'A web app that allows users to create and save maps to display points of interest.',
+    image: '/wiki-maps.png'
+  }],
+  bio: 'Full-Stack Web Developer with over 10 years of experience in project management. Looking to combine my passion for programming with skills in problem solving, creative thinking, collaboration, and communication.'
+}
+
+export default portfolio;
