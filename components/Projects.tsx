bc002a37578fa12dbe2180a87cbee2f3a7a26d93
Ryan <ryanwfortin@gmail.com>
2023-05-05 16:59:08 -0700

    add click behaviour

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,19 +1,46 @@
 import portfolio from "@/data";
 import styles from "@/styles/Projects.module.css";
-import { Fragment } from "react";
+import { useState } from "react";
 
-export default function Projects() {
+export default function Projects(props: any) {
+  const [activeProject, setActiveProject] = useState(false);
+  const [newIndex, setNewIndex] = useState(null);
+
+  const clickProject = function (index: any, projectKeys: any) {
+    setNewIndex(index);
+    setActiveProject(!activeProject);
+
+    !projectKeys.open ? (projectKeys.open = true) : (projectKeys.open = false);
+    console.log(projectKeys.open);
+  };
+
   return (
     <div className={styles.projectList}>
-      {portfolio.projects.map((projectKeys) => {
+      {portfolio.projects.map((projectKeys, index) => {
         return (
-          <div className={styles.project}>
+          <div
+            className={
+              projectKeys.open
+                ? styles.projectClicked
+                : styles.project
+            }
+            key={projectKeys.projectName}
+            onClick={() => {
+              clickProject(index, projectKeys);
+            }}
+          >
             <div className={styles.headline}>
               <div className={styles.headlineText}>
                 {projectKeys.projectName}
               </div>
             </div>
-            <div className={styles.content}>
+            <div
+              className={
+                projectKeys.open
+                  ? styles.contentClicked
+                  : styles.content
+              }
+            >
               <img
                 className={styles.projectImage}
                 src={projectKeys.image}
@@ -21,11 +48,11 @@ export default function Projects() {
               />
               <div className={styles.contentRightColumn}>
                 <div className={styles.about}>{projectKeys.about}</div>
-                <div className={styles.techStack}> 
+                <div className={styles.techStack}>
                   {projectKeys.techImages.map((projectTech, index) => {
                     const techWords = projectKeys.tech[index];
                     return (
-                      <div className={styles.techLogoLanguage}>
+                      <div className={styles.techLogoLanguage} key={index}>
                         <img
                           className={styles.tech}
                           src={projectTech}
