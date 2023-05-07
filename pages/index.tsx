874d888cb62392f86263b16a9e5006eff3b8491f
Ryan <ryanwfortin@gmail.com>
2023-05-06 19:54:28 -0700

    format changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,11 @@ import Head from "next/head";
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Ryan Fortin</title>
-      </Head>
-      <Intro />
-      <Projects />
+        <Head>
+          <title>Ryan Fortin</title>
+        </Head>
+        <Intro />
+        <Projects />
     </>
   );
 }
