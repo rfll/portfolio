1256cf4bf2e37146b382ee220367751b8e2e42e5
Ryan <ryanwfortin@gmail.com>
2023-05-07 14:20:46 -0700

    change to button html tag

--- components/IndividualButton.tsx
+++ components/IndividualButton.tsx
@@ -1,7 +1,7 @@
 import styles from "@/styles/IndividualButton.module.css"
 
-export default function IndividualButton() {
+export default function IndividualButton(props: any) {
   return (
-    <div className={styles.button}></div>
+    <button className={styles.button} onClick={props.onClick}>{!props.expand ? <div className={styles.buttonText}>See More</div> : <div className={styles.buttonText}>See Less</div>}</button>
   )
 } 
