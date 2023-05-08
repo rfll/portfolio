83fc09b4cb124e8a49a4e2dfe8586749f80646c2
Ryan <ryanwfortin@gmail.com>
2023-05-08 01:01:46 -0700

    adjust project about info

--- data.ts
+++ data.ts
@@ -4,7 +4,7 @@ const portfolio = {
     url: 'https://github.com/rfll/mocks',
     techImages: ['techImages/MongoDB.svg', 'techImages/ExpressJS.svg', 'techImages/React.svg', 'techImages/NodeJS.svg'],
     tech: ['MongoDB', 'ExpressJS', 'ReactJS', 'NodeJS'],
-    about: 'A web app that allows amateur basketball scouts to create a ranking of players entering the NBA.',
+    about: 'Drafted provides basketball fans with a comprehensive resource to identify and rank their favorite players leading up to the NBA Draft. Users will have access to a database of hundreds of players, player stats, and scouting videos to assist.',
     image: 'draftedDocs/drafted.png',
     video: "draftedDocs/Drafted App Demo.mp4",
     screenshots: null
@@ -14,7 +14,7 @@ const portfolio = {
     url: 'https://github.com/rfll/parrot-perch',
     techImages: ['techImages/PostgreSQL.svg', 'techImages/ExpressJS.svg', 'techImages/React.svg', 'techImages/NodeJS.svg'],
     tech: ['PostgreSQL', 'ExpressJS', 'ReactJS', 'NodeJS'],
-    about: 'A web app with interactive chat rooms where users can watch Twitch and Youtube streams, and play games.',
+    about: 'Parrot Perch is a single page application with interactive chat rooms where users can watch Twitch and Youtube streams, and play games.',
     image: 'parrotPerchDocs/parrot-perch.png',
     video: null,
     screenshots: ['parrotPerchDocs/parrotPerchLand.png', 'parrotPerchDocs/parrotPerchWhiteboard.png', 'parrotPerchDocs/parrotPerchYoutube.png', 'parrotPerchDocs/parrot-perch.png']
@@ -24,7 +24,7 @@ const portfolio = {
     url: 'https://github.com/rfll/scheduler',
     techImages: ['techImages/React.svg', 'techImages/NodeJS.svg', 'techImages/axios.png'],
     tech: ['ReactJS', 'NodeJS', 'Axios'],
-    about: 'A scheduling app that can create, edit, and delete interview appointments.',
+    about: 'Interview Scheduler is a single page application where users can book interviews between Monday to Friday. Interviews are booked by typing in a student name and clicking on an interviewer from a list of available interviewers. Interviews can be edited, deleted and cancelled.',
     image: 'schedulerDocs/scheduler.png',
     video: null,
     screenshots: ['schedulerDocs/bookScheduler.png', 'schedulerDocs/scheduled.png', 'schedulerDocs/deleteScheduler.png', 'schedulerDocs/scheduler.png']
