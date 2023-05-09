10300c795ba023a1aa437bfb3949e4f3ba13c3e0
Ryan <ryanwfortin@gmail.com>
2023-05-09 12:47:13 -0700

    use react context for intro component

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,48 +1,9 @@
-import portfolio from "@/data";
 import styles from "@/styles/Intro.module.css";
-import { useState, useEffect } from "react";
+import { useContext } from "react";
+import { portfolioContext } from "@/providers/PortfolioProvider";
 
 export default function Intro() {
-  const [index, setIndex] = useState(0);
-  const [activeClass, setActiveClass] = useState({ fade: "fadeIn" });
-
-  const phraseArray = portfolio.descriptions;
-
-  const fadeMilliseconds = 2000;
-  const arrayMilliseconds = fadeMilliseconds * 2;
-
-  if (typeof document !== "undefined") {
-    document.addEventListener("visibilitychange", () => {
-      if (document.visibilityState === "hidden") {
-        return;
-      }
-
-      if (document.visibilityState === "visible") {
-        setActiveClass({ fade: "fadeIn" });
-        setIndex((index + 1) % phraseArray.length);
-        return;
-      }
-    });
-  }
-
-  useEffect(() => {
-    if (document.visibilityState === "hidden") {
-      return;
-    }
-    const fadeTimer = setTimeout(() => {
-      activeClass.fade === "fadeIn" && setActiveClass({ fade: "fadeOut" });
-    }, fadeMilliseconds);
-  }, [activeClass]);
-
-  useEffect(() => {
-    if (document.visibilityState === "hidden") {
-      return;
-    }
-    const arrayTimer = setTimeout(() => {
-      setActiveClass({ fade: "fadeIn" });
-      setIndex((index + 1) % phraseArray.length);
-    }, arrayMilliseconds);
-  }, [index]);
+  const { activeClass, index, phraseArray } = useContext(portfolioContext);
 
   return (
     <main className={styles.main}>
