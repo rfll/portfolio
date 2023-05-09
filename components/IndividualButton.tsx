7f0c8a65e2722adb4713574e235f587bc3bd4bd0
Ryan <ryanwfortin@gmail.com>
2023-05-08 19:27:07 -0700

    add type checking for props

--- components/IndividualButton.tsx
+++ components/IndividualButton.tsx
@@ -1,7 +1,32 @@
-import styles from "@/styles/IndividualButton.module.css"
+import styles from "@/styles/IndividualButton.module.css";
 
-export default function IndividualButton(props: any) {
+type ButtonProps = {
+  expand: boolean;
+  onClick: () => void
+}
+
+export default function IndividualButton(props: ButtonProps) {
   return (
-    <button className={styles.button} onClick={props.onClick}>{!props.expand ? <div className={styles.buttonText}>See More<img className={styles.buttonArrow} src='chevrons-down.svg' alt="arrow"/></div> : <div className={styles.buttonText}>See Less<img className={styles.buttonArrow} src='chevrons-up.svg' alt="arrow"/></div>}</button>
-  )
-} 
+    <button className={styles.button} onClick={props.onClick}>
+      {!props.expand ? (
+        <div className={styles.buttonText}>
+          See More
+          <img
+            className={styles.buttonArrow}
+            src="chevrons-down.svg"
+            alt="arrow"
+          />
+        </div>
+      ) : (
+        <div className={styles.buttonText}>
+          See Less
+          <img
+            className={styles.buttonArrow}
+            src="chevrons-up.svg"
+            alt="arrow"
+          />
+        </div>
+      )}
+    </button>
+  );
+}
