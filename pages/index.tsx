f60d975877f80c26bf6aa73f4fa8fc10faac878b
Ryan <ryanwfortin@gmail.com>
2023-05-10 01:16:22 -0700

    add anchor id for selector

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
 import Intro from "@/components/Intro";
+import Nav from "@/components/Nav";
 import Projects from "@/components/Projects";
 import Head from "next/head";
+import styles from "@/styles/Projects.module.css"
 
 export default function Home() {
   return (
     <>
-        <Head>
-          <title>Ryan Fortin</title>
-        </Head>
-        <Intro />
-        <Projects />
+      <Head>
+        <title>Ryan Fortin</title>
+      </Head>
+      <Nav />
+      <Intro />
+      <div id="projectAnchor" className={styles.projectAnchor}></div>
+      <Projects />
     </>
   );
 }
