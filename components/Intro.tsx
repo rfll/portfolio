9e8f14f714e486cc9d946c199f8f7be350bcb9af
Ryan <ryanwfortin@gmail.com>
2023-05-10 16:22:20 -0700

    implement react-intersection observer

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,12 +1,20 @@
 import styles from "@/styles/Intro.module.css";
 import { useContext } from "react";
 import { portfolioContext } from "@/providers/PortfolioProvider";
+import { useInView } from "react-intersection-observer";
 
 export default function Intro() {
-  const { activeClass, index, phraseArray } = useContext(portfolioContext);
+  const { activeClass, index, phraseArray, setSelected } = useContext(
+    portfolioContext
+  );
+  const [ref, inView] = useInView({
+    threshold: 0.8
+  });
+
+  inView && setSelected("off");
 
   return (
-    <main className={styles.main}>
+    <main className={styles.main} ref={ref}>
       <div className={styles.profile}>
         <div className={styles.name}></div>
         <div className={styles[activeClass.fade]}>{phraseArray[index]}</div>
