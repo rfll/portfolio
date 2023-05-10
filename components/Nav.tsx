ae25780bd6b65953bc26bbcaffb630feae6cd7a3
Ryan <ryanwfortin@gmail.com>
2023-05-10 01:15:42 -0700

    build nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,58 @@
-import styles from '@/styles/Nav.module.css'
+import styles from "@/styles/Nav.module.css";
+import portfolio from "@/data";
+import { useContext, useState } from "react";
+import { portfolioContext } from "@/providers/PortfolioProvider";
+import { useInView } from "react-intersection-observer";
 
 export default function Nav() {
+  const { selected, setSelected } = useContext(portfolioContext);
+  const [ref, inView] = useInView()
+  const contactIcons = portfolio.contactIcons;
+
   return (
-    <div className="Nav">R</div>
-  )
+    <div className={styles.nav}>
+      <div className={styles.leftNav}>
+        <div
+          className={
+            selected === "projects"
+              ? styles.projectsNavClicked
+              : styles.projectsNav
+          }
+          onClick={() => {
+            document
+              .getElementById("projectAnchor")
+              ?.scrollIntoView({ behavior: "smooth" });
+          }}
+        >
+          Projects
+        </div>
+        <div
+          className={
+            selected === "about" ? styles.aboutNavClicked : styles.aboutNav
+          }
+          onClick={() => setSelected("about")}
+        >
+          About
+        </div>
+      </div>
+      <div
+        className={styles.header}
+        onClick={() => {
+          setSelected("off");
+          scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+          });
+        }}
+      >
+        Ryan Fortin
+      </div>
+      <div className={styles.rightNav}>
+        {contactIcons.map((icons) => {
+          return <img className={styles.icons} src={icons} alt="contactIcon" />;
+        })}
+      </div>
+    </div>
+  );
 }
