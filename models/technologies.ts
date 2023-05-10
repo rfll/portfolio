f3b781df226f198f286ba8e18ae36608a4078e09
Ryan <ryanwfortin@gmail.com>
2023-05-10 16:24:22 -0700

    create technologies enum

---
+++ models/technologies.ts
@@ -0,0 +1,11 @@
+enum Technologies {
+  MONGO_DB = "MONGO_DB",
+  NODE_JS = "NODE_JS",
+  EXPRESS_JS = "EXPRESS_JS",
+  REACT_JS = "REACT_JS",
+  POSTGRESQL = "POSTGRESQL",
+  AXIOS = "AXIOS",
+  JAVASCRIPT = "JAVASCRIPT"
+}
+
+export default Technologies
