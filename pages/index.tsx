eb259a81fbdb4a7684b67c07b06b6498420dd6f4
Ryan <ryanwfortin@gmail.com>
2023-05-10 16:24:40 -0700

    import about component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Nav from "@/components/Nav";
 import Projects from "@/components/Projects";
 import Head from "next/head";
 import styles from "@/styles/Projects.module.css"
+import About from "@/components/About";
 
 export default function Home() {
   return (
@@ -14,6 +15,8 @@ export default function Home() {
       <Intro />
       <div id="projectAnchor" className={styles.projectAnchor}></div>
       <Projects />
+      <div id="aboutAnchor" className={styles.projectAnchor}></div>
+      <About/>
     </>
   );
 }
