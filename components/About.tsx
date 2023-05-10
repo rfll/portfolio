0ed12757fb2afbec660edde1809f48f0e2df7e3c
Ryan <ryanwfortin@gmail.com>
2023-05-10 16:21:18 -0700

    create about component

---
+++ components/About.tsx
@@ -0,0 +1,15 @@
+import { portfolioContext } from "@/providers/PortfolioProvider";
+import styles from "@/styles/About.module.css";
+import { useContext } from "react";
+import { useInView } from "react-intersection-observer";
+
+export default function About() {
+  const { setSelected } = useContext(portfolioContext);
+  const [ref, inView] = useInView({
+    threshold: 0.8
+  });
+
+  inView && setSelected("about");
+
+  return <div className={styles.about} ref={ref}></div>;
+}
