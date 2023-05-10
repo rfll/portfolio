9f7be323e5e509effde0e95628f229b6d5682029
Ryan <ryanwfortin@gmail.com>
2023-05-10 16:21:40 -0700

    refactor projectprops

--- components/IndividualProject.tsx
+++ components/IndividualProject.tsx
@@ -1,16 +1,12 @@
 import { useState, useContext } from "react";
 import styles from "@/styles/Projects.module.css";
 import IndividualButton from "./IndividualButton";
+import TechnologyRow from "./TechnologyRow";
+import Technologies from "@/models/technologies";
+import Project from "@/models/project";
 
 type ProjectProps = {
-  projectName: string;
-  url: string;
-  techImages: string[];
-  tech: string[];
-  about: string;
-  image: string;
-  video: string | null;
-  screenshots: string[];
+  project: Project;
 };
 
 export default function IndividualProject(props: ProjectProps) {
@@ -19,52 +15,38 @@ export default function IndividualProject(props: ProjectProps) {
   return (
     <div
       className={expand ? styles.projectClicked : styles.project}
-      key={props.projectName}
+      key={props.project.projectName}
     >
       <div className={styles.headline}>
-        <div className={styles.headlineText}>
-          {props.projectName}
-        </div>
+        <div className={styles.headlineText}>{props.project.projectName}</div>
       </div>
       <div className={expand ? styles.contentClicked : styles.content}>
         <div className={styles.contentTopRow}>
           <img
             className={styles.projectImage}
-            src={props.image}
+            src={props.project.image}
             alt="image"
           />
           <div className={styles.contentRightColumn}>
-            <div className={styles.about}>{props.about}</div>
+            <div className={styles.about}>{props.project.about}</div>
             <div className={styles.techStack}>
-              {props.techImages.map(
-                (projectTech: string, index: number) => {
-                  const techWords = props.tech[index];
-                  return (
-                    <div className={styles.techLogoLanguage} key={index}>
-                      <img
-                        className={styles.tech}
-                        src={projectTech}
-                        alt="tech"
-                      />
-                      <div className={styles.techName}>{techWords}</div>
-                    </div>
-                  );
-                }
-              )}
+              {props.project.technologies.map((tech) => (
+                <TechnologyRow technology={tech} />
+              ))}
             </div>
           </div>
         </div>
         {expand && (
           <div className={styles.contentBottomRow}>
-            {props.video ? (
+            {props.project.video ? (
               <video
                 className={styles.videoPlayer}
-                src={props.video}
+                src={props.project.video}
                 controls={true}
               ></video>
             ) : (
               <div className={styles.screenshotGrid}>
-                {props.screenshots.map((screenshot: string) => {
+                {props.project.screenshots.map((screenshot: string) => {
                   return (
                     <img
                       className={styles.projectScreenshot}
