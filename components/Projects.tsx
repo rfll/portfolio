3e3d03a13d77f0c6986927bdc64b9f945c08f257
Ryan <ryanwfortin@gmail.com>
2023-05-10 18:54:30 -0700

    calculate projectList height for inView threshold

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,25 +6,29 @@ import { useContext } from "react";
 import { portfolioContext } from "@/providers/PortfolioProvider";
 import Project from "@/models/project";
 
-
 export default function Projects() {
+  const thresholdBaseline = 0.7;
+  let threshold;
+  if (typeof document !== "undefined") {
+    let elem = document.getElementById("projectList") as HTMLInputElement;
+    let elemHeight = elem.getBoundingClientRect().height;
+    threshold =
+      ((window.innerHeight * thresholdBaseline) / elemHeight) *
+      thresholdBaseline;
+  }
   const { setSelected } = useContext(portfolioContext);
   const [ref, inView] = useInView({
-    threshold: 0.376,
+    threshold: threshold,
   });
   const projects = portfolio.projects;
+
 
   inView && setSelected("projects");
 
   return (
     <div className={styles.projectList} ref={ref} id="projectList">
       {projects.map((project: Project, index: number) => {
-        return (
-          <IndividualProject
-            project={project}
-            key={index}
-          />
-        );
+        return <IndividualProject project={project} key={index} />;
       })}
     </div>
   );
